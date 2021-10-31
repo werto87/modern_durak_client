4b301bac44930499cf8a4196c4365647a066f109
werto87 <huhulasmichinruhe@web.de>
2021-10-31 12:04:20 +0100

    added commented out local websocket address for easier switch to debugging

--- src/Webservice/store.js
+++ src/Webservice/store.js
@@ -3,6 +3,7 @@ import { writable } from 'svelte/store';
 export const messageStore = writable('');
 
 export const webSocket = new WebSocket('wss://modern-durak.com/wss');
+// export const webSocket = new WebSocket('wss://localhost:55555');
 
 // Connection opened
 webSocket.addEventListener('open', function (event) {
