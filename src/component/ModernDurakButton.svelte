b54b65c8ce917711d4d95b8325b4a86f7a8292f0
werto87 <huhulasmichinruhe@web.de>
2022-11-06 22:04:59 +0900

    communicate button disabled

--- src/component/ModernDurakButton.svelte
+++ src/component/ModernDurakButton.svelte
@@ -5,6 +5,7 @@
     export let disabled = undefined;
 
 </script>
-<button disabled={disabled} type={type} on:click={onClick} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+<button disabled={disabled} type={type} on:click={onClick}
+        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none disabled:opacity-25">
     {buttonText}
 </button>
